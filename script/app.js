0d45cb35b35bd745ee6984b6ad42fe96d48a7627
mohammadi <designs.by.golrokh@gmail.com>
2025-09-20 10:59:40 -0800

    start

---
+++ script/app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// const container = document.querySelector(".container");
+
+// window.onmousemove = function (e) {
+//     let x = e.clientX/5;
+//     let y = e.clientY/5;
+//     container.style.backgroundPositionX = x + "px";
+//     container.style.backgroundPositionY = y + "px";
+// }
+
+const container = document.querySelector(".container");
+window.onmousemove = function (e) {
+    let x = e.clientX/5;
+    let y = e.clientY/5;
+    container.style.backgroundPositionX = x + "px";
+    container.style.backgroundPositionY = y + "px";
+    
+}
